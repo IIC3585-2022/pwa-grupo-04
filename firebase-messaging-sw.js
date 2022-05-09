333d4a6d5c39dd68d5fbe05a40b122739ee2cceb
FloValdes <ftvaldes1@uc.cl>
2022-05-08 22:17:44 -0400

    fix(): notifications

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,39 @@
+importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
+);
+// For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
+);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkP8LeGANbsy3z0OiQXln_THepQAtYMZ0",
+  authDomain: "iic3585-g4-e4.firebaseapp.com",
+  projectId: "iic3585-g4-e4",
+  storageBucket: "iic3585-g4-e4.appspot.com",
+  messagingSenderId: "216241501064",
+  appId: "1:216241501064:web:b8540bbf99960d3e52403d"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log(
+        "[firebase-messaging-sw.js] Received background message ",
+        payload,
+    );
+    // Customize notification here
+    const notificationTitle = "Background Message Title";
+    const notificationOptions = {
+        body: "Background Message body.",
+        icon: "/itwonders-web-logo.png",
+    };
+
+    return self.registration.showNotification(
+        notificationTitle,
+        notificationOptions,
+    );
+});
